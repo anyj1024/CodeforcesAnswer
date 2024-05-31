b45936e3d6658d2b20831c781c5e34d21568d169
Yongjie An <anyj1024@qq.com>
2024-05-31 23:44:06 +0800

    feat: edu166-a

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int v = Integer.parseInt(br.readLine());
+        while (v-- != 0) {
+            int n = Integer.parseInt(br.readLine());
+            char[] cs = br.readLine().toCharArray();
+
+            System.out.println(getAns(cs, n));
+        }
+    }
+
+    private static String getAns(char[] cs, int n) {
+        boolean hasAlphaDigit = false;
+        char lastChar = 0;
+
+        for (int i = 0; i < cs.length; i++) {
+            if ((Character.isDigit(cs[i]) && hasAlphaDigit) || cs[i] < lastChar) {
+                return "NO";
+            }
+            lastChar = cs[i];
+        }
+
+        return "YES";
+    }
+}
