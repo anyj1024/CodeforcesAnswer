f1b931567d603e7194fb4837e198bd844a19c1de
Yongjie An <anyj1024@qq.com>
2024-06-01 23:52:36 +0800

    feat: cf949-a

--- src/CFRound949Div2/A.java
+++ src/CFRound949Div2/A.java
@@ -1,9 +1,12 @@
-import java.util.*;
-import java.io.*;
+package CFRound949Div2;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    public static void main(String[] args) throws IOException{
+public class A {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int v = Integer.parseInt(br.readLine());
         while (v-- != 0) {
